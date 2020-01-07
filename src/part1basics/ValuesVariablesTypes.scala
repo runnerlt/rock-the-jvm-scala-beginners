094c318be365ed5832fe971eccead9228e0dccfd
juozas <juozas.pastas@gmail.com>
2020-01-08 00:13:25 +0200

    Lesson 2

--- src/part1basics/ValuesVariablesTypes.scala
+++ src/part1basics/ValuesVariablesTypes.scala
@@ -1,5 +1,22 @@
 package part1basics
 
-object ValuesVariablesTypes extends App{
+object ValuesVariablesTypes extends App {
+  val x = 42
+  println(x)
+  //  x = 2
+  val aString: String = "hello"
+  val anotherString: String = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aInt: Int = x
+  val aShort: Short = 4136
+  val aLong: Long = 564645463543434L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+  aVariable = 5
 
 }
