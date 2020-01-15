94f25e47f2bf5791a2278e37ed477070211221ea
juozas <juozas.pastas@gmail.com>
2020-01-15 21:53:17 +0200

    Methos notations: 1/4

---
+++ src/lectures/part2oop/ScalaObjects.scala
@@ -0,0 +1,21 @@
+package lectures.part2oop
+
+object ScalaObjects extends App {
+
+  val pp = Person("Kostas")
+  println(pp.talkClass())
+}
+
+object Person {
+  val name: String = "Juozas"
+  val age: Int = 35
+
+  def talk(): String = s"My name is $name"
+
+  def apply(name: String) : Person = new Person(name, age)
+
+}
+
+class Person(name: String, age: Int) {
+  def talkClass(): String = s"Ok, Im $name and my age is ${Person.age}"
+}
