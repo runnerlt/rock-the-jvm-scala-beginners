8eb4e97c9022ebe0950470abbede3f66ef9fc108
s9589b <juozas.pastas@gmail.com>
2020-01-29 23:54:55 +0200

    Implementing MyList using Generics, finished

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -2,9 +2,28 @@ package exercises
 
 
 object MyListGeneric extends App {
-  val intList: Cons[Int] = new Cons(5, Empty)
-  println(intList.head())
-  println(intList.toString())
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
+
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString())
+
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(elem: Int): Int = elem * 2
+  }))
+
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(elem: Int): Boolean = elem % 2 == 0
+  }))
+
+  println((listOfIntegers ++ anotherListOfIntegers).toString)
+
+  println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
+    override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons[Int](elem + 1, Empty))
+  }))
+
 }
 
 abstract class MyList[+A] {
@@ -20,10 +39,15 @@ abstract class MyList[+A] {
 
   override def toString: String = "[" + printElements + "]"
 
-  // signatures of map, flaytMap, filter
+  // signatures of map, flatMap, filter
   def map[B](transformer: MyTransformer[A, B]): MyList[B]
+
   def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+
   def filter(predicate: MyPredicate[A]): MyList[A]
+
+  //  concatenation
+  def ++[B >: A](list: MyList[B]): MyList[B]
 
 }
 
@@ -37,6 +61,14 @@ object Empty extends MyList[Nothing] {
   override def add[B >: Nothing](item: B): MyList[B] = new Cons(item, Empty)
 
   override def printElements: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -53,10 +85,40 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     else h + " " + t.printElements
   }
 
+  /** Performs transformation on linked list MyList
+   * Takes type B as parameter (target type of MyList to be returned)
+   * Transforms HEAD position, and calls self recursively on the TAIL to transform all remaining elements of tail
+   * And of course: all this recursion result is wrapped into a new Cons object of return type B
+   *
+   * @param transformer MyTransformer type with a transform():B method from A to B
+   * @tparam B - type of MyList to be returned
+   * @return linked list MyList of type B
+   */
+  def map[B](transformer: MyTransformer[A, B]): MyList[B] = {
+    new Cons(transformer.transform(h), t.map(transformer))
+  }
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+  /** Performs filtering on linked list MyList
+   * checks of HEAD meets criteria, if yes - returns new Cons object with this head and recursively calling
+   * self on the tail.
+   * If head does not meets criteria, just calling recursively self on the tail
+   *
+   * @param predicate of type MyPredicate[A] with method test: boolean
+   * @return MyList of type A, filtered by predicate
+   */
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  // helper method to concatenate current list with given list
+  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ (list))
 }
 
 // traits predicate, MyTransformer
-trait MyPredicate[-T]{
+trait MyPredicate[-T] {
   def test(elem: T): Boolean
 }
 
