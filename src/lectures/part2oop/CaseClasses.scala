af41955a2f1d2011accc65535396dcdbc20df98c
s9589b <juozas.pastas@gmail.com>
2020-02-01 13:26:09 +0200

    Case classes

--- src/lectures/part2oop/CaseClasses.scala
+++ src/lectures/part2oop/CaseClasses.scala
@@ -5,4 +5,24 @@ object CaseClasses extends App {
   // experiments with case classes and objects.
   // test all useful features
 
+  val j = new Person("juozas", 35)
+  val j2 = new Person("juozas", 35)
+  val j3 = j.copy(age = 89)
+  val pObj = Person("tom", 10)
+
+  //  println(j.name)
+  //  println(j == j2)
+  //  println(j3)
+
+  case class Person(name: String, age: Int) {
+    println("just saying, that im executing on initialization")
+
+    def intro(): String = {
+      s"Im here with name $name"
+    }
+  }
+
+  println(pObj)
+
+
 }
