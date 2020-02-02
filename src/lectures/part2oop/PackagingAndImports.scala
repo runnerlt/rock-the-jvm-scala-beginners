7ca3941f239176a76a26eba8cc301e2b55198c6a
s9589b <juozas.pastas@gmail.com>
2020-02-02 13:37:41 +0200

    Packaging

---
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,13 @@
+package lectures.part2oop
+
+import playground.{Cindirella => Princess, PrinceCharming}
+
+object PackagingAndImports extends App {
+
+  //  val e = new Writer()
+
+  val c = new Princess
+  val ch = new PrinceCharming
+  println(myName)
+
+}
