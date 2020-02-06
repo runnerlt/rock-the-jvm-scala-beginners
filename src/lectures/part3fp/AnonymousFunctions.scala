5497f559b4d9b558ee267970051e140b30c5361a
s9589b <juozas.pastas@gmail.com>
2020-02-06 22:41:40 +0200

    Anonymous functions, lambdas

--- src/lectures/part3fp/AnonymousFunctions.scala
+++ src/lectures/part3fp/AnonymousFunctions.scala
@@ -2,4 +2,14 @@ package lectures.part3fp
 
 object AnonymousFunctions extends App {
 
+  def crap: (Int, Int) => Int = new Function2[Int, Int, Int] {
+    override def apply(x: Int, y: Int): Int = x * y
+  }
+
+  println(crap.apply(4, 5))
+
+
+  def oky: (Int, Int) => Int = _ * _
+
+  println(oky(5, 8))
 }
--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -2,12 +2,9 @@ package playground
 
 object ScalaPlayground extends App {
 
-  def fn: (Int, Int) => Int = new Function2[Int, Int, Int] {
-    override def apply(elem1: Int, elem2: Int): Int = elem1 * elem2
+  trait Test {
+    def call(x: Int): Int
   }
-
-  println(fn(5, 8))
-
 
 }
 
